e10702bca4d669c23ea9f8adc43203f6e5a1652f
Israel Ludolf <61331235+iLudolf@users.noreply.github.com>
2021-04-28 23:13:03 -0300

    Aula 02
    Build do projeto usando o SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,26 @@
+//SPA - Single Page Applications, ou (Aplicações de Página Única)
+
+//SSR - Server Side Rendering - (Rendering for server Node js) 
+//toda request será solicitada ao servidor ao servidor 
+
+//SSG - (Rendering for server Next js)
+// Define uma pagina estatica e todos os usuarios receberam essa pagina static em tempo x
+
 export default function Home() {
-  return (
+   return (
   <h1>Index</h1>
   );
+}
+
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return { 
+    props:{
+      episodes: data,
+    }, 
+    revalidate: 60 * 60 * 8, //tempo para que seja construido uma nova pagina static
+  }
 }
